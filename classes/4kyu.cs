0fc94f6902aca1a9a871d94b0a7ce384a7d6646d
oso082024 <oso082024@gmail.com>
2024-10-25 12:39:43 +0200

    FindMissingdigit(still unfin)

--- classes/4kyu.cs
+++ classes/4kyu.cs
@@ -1,16 +1,17 @@
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
 public class FourKyu
 {
     public int FindMissingDigit(string expression)
     {
-        string tempExpression = expression;
         int a; int b; int c;
         if (expression.Contains('+') || expression.Contains('*'))
         {
             for(int i = 0; i <10; i++)
-            {
-                //tempExpression = tempExpression.Replace('?', Convert.ToChar(Convert.ToString(i)));
-                string[] strArr = tempExpression.Replace('?', 
-                                                    Convert.ToChar(Convert.ToString(i)))
+            {    
+                string[] strArr = expression.Replace('?', Convert.ToChar(
+                                                              Convert.ToString(i)))
                                                 .Split('*', '=', '+');
             
                 a = Convert.ToInt32(strArr[0]);
@@ -22,7 +23,20 @@ public class FourKyu
         }
         else
         {
-            
+            for (int i = 0; i < 10; i++)
+            {
+                if (expression[0] == '-')
+                {
+                    string[] strArr = expression.Insert(expression.IndexOf('-', 
+                                                        expression.IndexOf('-') + 1), "=")
+                                                .Replace('?', Convert.ToChar(Convert.ToString(i)))
+                                                .Split('=');
+                }
+                else
+                {
+                    string[] strArr = expression.Insert(expression.IndexOf('-'), "=").Replace('?', Convert.ToChar(Convert.ToString(i)));
+                }
+            }
         }
         return -1;
     }
