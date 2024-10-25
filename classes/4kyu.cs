3e2c4b02e3da6bf11bf8c2c93970df235a6d271d
oso082024 <oso082024@gmail.com>
2024-10-25 13:52:05 +0200

    FindMissingDigit (unfin) need to check if 0 i the only number

--- classes/4kyu.cs
+++ classes/4kyu.cs
@@ -1,3 +1,4 @@
+using System.Formats.Asn1;
 using System.Linq.Expressions;
 using System.Security.Cryptography.X509Certificates;
 
@@ -5,38 +6,82 @@ public class FourKyu
 {
     public int FindMissingDigit(string expression)
     {
+        if (expression == "") return -1;
+        bool isFirstZero = true;
         int a; int b; int c;
-        if (expression.Contains('+') || expression.Contains('*'))
+        for(int i = 0; i <10; i++)
         {
-            for(int i = 0; i <10; i++)
-            {    
+            if (expression.Contains('+') || expression.Contains('*'))
+            { 
                 string[] strArr = expression.Replace('?', Convert.ToChar(
-                                                              Convert.ToString(i)))
+                                                            Convert.ToString(i)))
                                                 .Split('*', '=', '+');
-            
                 a = Convert.ToInt32(strArr[0]);
                 b = Convert.ToInt32(strArr[1]);
                 c = Convert.ToInt32(strArr[2]);
-                if(a + b == c && expression.Contains('+')) return i;
-                if(a * b == c && expression.Contains('*')) return i;
             }
-        }
-        else
-        {
-            for (int i = 0; i < 10; i++)
+            else
             {
                 if (expression[0] == '-')
-                {
-                    string[] strArr = expression.Insert(expression.IndexOf('-', 
-                                                        expression.IndexOf('-') + 1), "=")
-                                                .Replace('?', Convert.ToChar(Convert.ToString(i)))
-                                                .Split('=');
+                {   
+                    while (isFirstZero)
+                    {
+                        isFirstZero = false;
+                        string[] strArr = expression.Insert(expression.IndexOf('-', 
+                                                            expression.IndexOf('-') + 1), "=")
+                                                    .Replace('?', Convert.ToChar(Convert.ToString(i)))
+                                                    .Split('=');
+                        if (strArr[0][0] == '0' || strArr[1][0] == '0' || strArr[2][0] == '0')
+                        {
+                            isFirstZero = true;
+                            i++;
+                        }
+                        else if (strArr[0][0] == '-' || strArr[1][0] == '-' || strArr[2][0] == '-')
+                        {
+                            if (strArr[0][1] == '0' || strArr[1][1] == '0' || strArr[2][1] == '0')
+                            {
+                                isFirstZero = true;
+                                i++;
+                            }
+                        }
+
+                        a = Convert.ToInt32(strArr[0]);
+                        b = Convert.ToInt32(strArr[1].Remove(0,1));
+                        c = Convert.ToInt32(strArr[2]); 
+                    }
                 }
                 else
                 {
-                    string[] strArr = expression.Insert(expression.IndexOf('-'), "=").Replace('?', Convert.ToChar(Convert.ToString(i)));
-                }
+                    while (isFirstZero)
+                    {
+                        isFirstZero = false;
+                        string[] strArr = expression.Insert(expression.IndexOf('-'), "=")
+                                                    .Replace('?', Convert.ToChar(Convert.ToString(i)))
+                                                    .Split('=');
+                        if (strArr[0][0] == '0' || strArr[1][0] == '0' || strArr[2][0] == '0')
+                        {
+                            isFirstZero = true;
+                            i++;
+                        }
+                        else if (strArr[0][0] == '-' || strArr[1][0] == '-' || strArr[2][0] == '-')
+                        {
+                            if (strArr[0][1] == '0' || strArr[1][1] == '0' || strArr[2][1] == '0')
+                            {
+                                isFirstZero = true;
+                                i++;
+                            }
+                        }
+
+                        a = Convert.ToInt32(strArr[0]);
+                        b = Convert.ToInt32(strArr[1].Remove(0,1));
+                        c = Convert.ToInt32(strArr[2]); 
+                    }                                             
+                } 
             }
+
+            if(a + b == c && expression.Contains('+')) return i;
+            if(a * b == c && expression.Contains('*')) return i;
+            if(a - b == c && expression.Contains('-')) return i;
         }
         return -1;
     }
--- obj/Debug/net8.0/cw.AssemblyInfo.cs
+++ obj/Debug/net8.0/cw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5b96089c979165fa84bf58f9f13c3c76d02311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc94f6902aca1a9a871d94b0a7ce384a7d6646d")]
 [assembly: System.Reflection.AssemblyProductAttribute("cw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
