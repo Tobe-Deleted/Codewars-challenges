4a67a2db44a344d1f9af880e18d1307ac3ec5f05
oso082024 <oso082024@gmail.com>
2025-02-18 14:31:45 +0100

    Interface added to Closest()

--- Interface/I5kyu.cs
+++ Interface/I5kyu.cs
@@ -7,4 +7,12 @@ public interface I5kyu
     /// <param name="arr"></param>
     /// <returns>new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1} => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}</returns>
     public int[] MoveZeroes(int[] arr);
+
+    /// <summary>
+    /// Takes in a string of numbers seperated by space. Finds the weight(all digits added together) and returns the two closest numbers by weight.
+    /// https://www.codewars.com/kata/5868b2de442e3fb2bb000119/csharp
+    /// </summary>
+    /// <param name="strng"></param>
+    /// <returns>An array with two intArrays containing weight, position, number of the two closest numbers by weight</returns>
+    public int[][] Closest(string strng);
 }
