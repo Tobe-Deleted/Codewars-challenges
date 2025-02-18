e33f3ea345ed13a5c654c69e461432bd0d0c3ede
oso082024 <oso082024@gmail.com>
2025-02-18 10:55:18 +0100

    Closest() finished

--- classes/5kyu.cs
+++ classes/5kyu.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 public class FiveKyu : I5kyu
 {
     public int[] MoveZeroes(int[] arr)
@@ -24,8 +25,10 @@ public class FiveKyu : I5kyu
 
     public int[][] Closest(string strng) 
     {
+        if(strng == "") return new int[0][];
         int[][] result = new int[2][];
         string[] numbers = strng.Split(' ');
+        int[] sortedWeights = new int[numbers.Length];
         int[] weights = new int[numbers.Length];
         int i = 0;
         foreach(string str in numbers)
@@ -35,19 +38,27 @@ public class FiveKyu : I5kyu
             {
                 weight += Convert.ToInt32(ch) -48;
             }
+            sortedWeights[i] = weight;
             weights[i] = weight;
             i++;
         }
         int[] first = new int[3];
         int[] second = new int[3];
-        int[] sortedWeights = weights;
         Array.Sort(sortedWeights);
         for(int n = 1; n < weights.Length -1; n++)
         {
             if(Math.Abs(sortedWeights[n-1] - sortedWeights[n]) < Math.Abs(first[0] - second[0]) || first[0] + second[0] == 0)
             {
                 int firstNumberIndex = Array.IndexOf(weights,sortedWeights[n-1]);
-                int secondNumberIndex = Array.IndexOf(weights,sortedWeights[n]);
+                int secondNumberIndex;
+                if(Math.Abs(sortedWeights[n-1] - sortedWeights[n]) == 0)
+                {
+                    secondNumberIndex = Array.IndexOf(weights,sortedWeights[n-1], firstNumberIndex  + 1 );
+                }
+                else
+                {
+                    secondNumberIndex = Array.IndexOf(weights,sortedWeights[n]);
+                }
                 first[0] = sortedWeights[n-1]; first[1] = firstNumberIndex; first[2] = Convert.ToInt32(numbers[firstNumberIndex]);
                 second[0] = sortedWeights[n]; second[1] = secondNumberIndex; second[2] = Convert.ToInt32(numbers[secondNumberIndex]);
             }
--- obj/Debug/net8.0/cw.AssemblyInfo.cs
+++ obj/Debug/net8.0/cw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c671bbbd51549f45fa4463d98b3c099b38ccc61d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5c4ee04a384a07ed8a369b2dad18655f9b79b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("cw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
