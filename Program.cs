e6176d8187dbc7c6c4781403ce94f448c3c9637c
Tobe-Deleted <oso082024@gmail.com>
2025-03-17 09:12:32 +0100

    Interpret() finished

--- Program.cs
+++ Program.cs
@@ -71,7 +71,7 @@ class Program
 
         FiveKyu kyu5 = new FiveKyu();
 
-        Dictionary<string, int> dict = kyu5.Interpret(new string[]{"mov a 5","inc a","dec a","dec a","jnz a -1","inc a","mov a -10","mov b a","inc a","dec b","jnz a -2","mov a 1","mov b 1","mov c 0","mov d 26","jnz c 2","jnz 1 5","mov c 7","inc d","dec c","jnz c -2","mov c a","inc a","dec b","jnz b -2","mov b c","dec d","jnz d -6","mov c 18","mov d 11","inc a","dec d","jnz d -2","dec c","jnz c -5","mov d 100","dec d","mov b d","jnz b -2","inc d","mov a d","jnz 5 10","mov c a"});
+        Dictionary<string, int> dict = kyu5.Interpret(new string[]{"mov d 100","dec d","mov b d","jnz b -2","inc d","mov a d","jnz 5 10","mov c a"});
         // Expected: { a: 318009, b: 196418, c: 0, d: 0 }, Actual: { a: 318009, b: 196418, c: 0, d: 0, 1: 1 }
         foreach(var kvp in dict)Console.WriteLine($"{kvp.Key}: {kvp.Value}");
         // int[] arr = {1, 0, 2, 0, 1, 0, 1, 0, 3, 1};
--- obj/Debug/net8.0/cw.AssemblyInfo.cs
+++ obj/Debug/net8.0/cw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc235bdae0650c5be514b5344fb240be12af234e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947aafcb5ee729a70cd6b6ad947b1b85bd0d8db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("cw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
